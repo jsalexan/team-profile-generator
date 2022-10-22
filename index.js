d6656315ce24335e0c82c49cd7984da1665a3640
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-10-22 14:09:56 -0700

    Added js files. Added code for the four main staff files. Started code for index.js. Pushing to document.

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employee = require("./lib/employee");
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+
