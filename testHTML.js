115e3a3c94c46aa3ec7c4936054c437a0b513596
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-10-25 20:33:52 -0700

    Still trying to figure this out. Now it won't create the document, but updated a lot of the HTML templates to try to get it closer to working with the 3 different employee types. I think the next thing I need to do is to update the for loop to check which employee we're looking for. I have some ideas, but will have to pick this up later. I also began looking into the bootstrap templates and added some pieces of that. I'll need to get a card rendering to see what I have and to make the necessary adjustments. Also added a few simple employee type graphics to play around with.

--- testHTML.js
+++ testHTML.js
@@ -2,32 +2,73 @@ function createHTML(totalTeam) {
    let cards = []
    for (let i=0; i<totalTeam.length; i++)
    {
-    let card = `
-    <h1>${totalTeam[i].name}</h1>
-    <p>${totalTeam[i].id}</p>
-    <p>${totalTeam[i].email}</p>
-    <p>${totalTeam[i].gitHubName}</p>
-    <p>${totalTeam[i].school}</p>
-    <p>${totalTeam[i].officeNumber}</p>
-
-    `
-    cards.push(card);
-   }
+    let managerCard = `
+      <div class="card" style="width: 18rem;">
+      <img src="./src/images/manager.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h1 class="card-title">${totalTeam.getName()}</h1>
+        <h2 class="card-text">${totalTeam.getRole()}</h2>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${totalTeam.getId()}</li>
+        <li class="list-group-item">${totalTeam.getEmail()}</li>
+        <li class="list-group-item">${totalTeam.getOfficeNumber()}</li>
+      </ul>
+    </div>
+       `
+       cards.push(employeeCard);
+    }
+   };
+    {
+      let engineerCard = `
+        <div class="card" style="width: 18rem;">
+        <img src="./src/images/engineer.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h1 class="card-title">${totalTeam.getName()}</h1>
+          <h2 class="card-text">${totalTeam.getRole()}</h2>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">${totalTeam.getId()}</li>
+          <li class="list-group-item">${totalTeam.getEmail()}</li>
+          <li class="list-group-item">${totalTeam.getGitHub()}</li>
+        </ul>
+      </div>
+         `
+      }
+      {
+         let internCard = `
+           <div class="card" style="width: 18rem;">
+           <img src="./src/images/intern.png" class="card-img-top" alt="Globe head with graduation cap">
+           <div class="card-body">
+             <h1 class="card-title">${totalTeam.getName()}</h1>
+             <h2 class="card-text">${totalTeam.getRole()}</h2>
+           </div>
+           <ul class="list-group list-group-flush">
+             <li class="list-group-item">${totalTeam.getId()}</li>
+             <li class="list-group-item">${totalTeam.getEmail()}</li>
+             <li class="list-group-item">${totalTeam.getSchool()}</li>
+           </ul>
+         </div>
+            `
+         }
+   createHTML();
 
    let html = `
+   <!DOCTYPE html>
    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    </head>
-    <body>
-    ${cards}
-    </body>
-    </html>
+
+   <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Our Team</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+   <body>
+   ${cards}
+   </body>
+   </html>
    `
    return html
-};
 
-module.exports = createHTML;
+
+module.exports = createHTML
